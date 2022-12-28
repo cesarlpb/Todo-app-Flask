d02db73841865480d9dcfb8f69e0c7beca606f0a
César P <cesarlpb@gmail.com>
2022-12-28 13:30:24 +0100

    Test db con SQLite3 - hasta vista Create

--- app/test_routes.py
+++ app/test_routes.py
@@ -1,7 +1,15 @@
-# routes.py
-
+# test_routes.py
+import sqlite3 as sql
 from test_app import app
 from flask import render_template, request
+
+# connect to qa_database.sq (database will be created, if not exist)
+db_name = 'test.db'
+db_table = 'test'
+con = sql.connect(db_name)
+con.execute(f'CREATE TABLE IF NOT EXISTS {db_table} (ID INTEGER PRIMARY KEY AUTOINCREMENT,'
+            + 'Question TEXT, Answer TEXT)')
+con.close()
 
 # home page
 @app.route('/')  # root : main page
@@ -15,13 +23,29 @@ def create():
     if request.method == 'GET':
         # send the form
         return render_template('test_create.html')
-    else: # request.method == 'POST':
+    elif request.method == 'POST':
         # read data from the form and save in variable
         question = request.form['question']
         answer = request.form['answer']
 
         # store in database
-        # add code here
+        try:
+            con = sql.connect(db_name)
+            c =  con.cursor() # cursor
+            # insert data
+            c.execute(f"INSERT INTO {db_table} (question, answer) VALUES (?,?)",
+                (question, answer))
+            con.commit() # apply changes
+            # go to thanks page
+            return render_template('test_create_thanks.html', question=question)
+        except con.Error as err: # if error
+            # then display the error in 'database_error.html' page
+            return render_template('test_db_error.html', error=err)
+        finally:
+            con.close() # close the connection
+        
+    else:
+        return 'Método no permitido', 405 # 400 de Bad Request o 405 de método no permitido
 
         return render_template('test_create_thanks.html', question=question)
 
